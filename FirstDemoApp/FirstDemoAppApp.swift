8986446190da8b05a843110dd851e701856581a4
Olanrewaju Michael Afolayan <michaelflourish@hotmail.com>
2024-02-18 08:55:53 -0500

    Initial Commit

---
+++ FirstDemoApp/FirstDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstDemoAppApp.swift
+//  FirstDemoApp
+//
+//  Created by Olanrewaju Afolayan on 2/18/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
